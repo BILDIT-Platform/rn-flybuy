5aa5d70ae650f3eebd38852012f2addff7dddfd6
Addin Gama Bertaqwa <addingama@gmail.com>
2024-07-18 22:32:45 +0800

    feat: customer related function on example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {SafeAreaView, ScrollView, useColorScheme} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {Button} from './components';
-import * as FlyBuyCore from '@bildit-platform/rn-flybuy-core';
+import {CustomerSection} from './CustomerSection';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -19,19 +11,12 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const login = () => {
-    FlyBuyCore.login('ha_zellat@esi.dz', 'password')
-      .then(customer => console.log(customer))
-      // .then(customer => console.tron.log('customer', customer))
-      .catch(err => console.error(err));
-  };
-
   return (
     <SafeAreaView style={backgroundStyle}>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Button title="Login" onPress={login} />
+        <CustomerSection />
       </ScrollView>
     </SafeAreaView>
   );
