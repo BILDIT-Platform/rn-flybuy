9dc462a4a7ddd47bf0fba7ed1fa2ed0063c5bf63
Addin Gama Bertaqwa <addingama@gmail.com>
2024-07-25 09:48:51 +0800

    feat: FlyBuy Pickup wrapper

--- mono/packages/pickup/android/src/main/java/com/bilditplatform/rnflybuypickup/RnFlybuyPickupModule.kt
+++ mono/packages/pickup/android/src/main/java/com/bilditplatform/rnflybuypickup/RnFlybuyPickupModule.kt
@@ -3,6 +3,7 @@ package com.bilditplatform.rnflybuypickup
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import com.radiusnetworks.flybuy.sdk.pickup.PickupManager
 
 class RnFlybuyPickupModule internal constructor(context: ReactApplicationContext) :
   RnFlybuyPickupSpec(context) {
@@ -11,11 +12,10 @@ class RnFlybuyPickupModule internal constructor(context: ReactApplicationContext
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  override fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  override fun onPermissionChanged(promise: Promise) {
+    PickupManager.getInstance().onPermissionChanged()
+    promise.resolve("Ok")
   }
 
   companion object {
--- mono/packages/pickup/android/src/newarch/RnFlybuyPickupSpec.kt
+++ mono/packages/pickup/android/src/newarch/RnFlybuyPickupSpec.kt
@@ -4,4 +4,5 @@ import com.facebook.react.bridge.ReactApplicationContext
 
 abstract class RnFlybuyPickupSpec internal constructor(context: ReactApplicationContext) :
   NativeRnFlybuyPickupSpec(context) {
+    abstract fun onPermissionChanged(promise: Promise)
 }
